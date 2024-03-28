5a1f1e104656bd6749d6d69edd1a5ea10c762774
Sarthak0203 <138149235+Sarthak0203@users.noreply.github.com>
2024-03-28 23:24:45 +0530

    updated cors

--- api/index.js
+++ api/index.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const app = express();
 app.use(express.json());
 app.use(cors({
-  origin: 'https://anonochat.vercel.app/',
+  origin: 'https://anonochat.vercel.app',
   credentials: true
 }));
 const user = require('./Models/User');
